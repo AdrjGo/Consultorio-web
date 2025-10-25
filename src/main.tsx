b31aec091d61b90f4e15c3aa1f650c009821e7ad
AdrjGo <gcadrianj@gmail.com>
2025-10-25 00:37:23 -0400

    feat: add Toast component for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer } from "react-toastify";
 import { RouterProvider } from "react-router";
 import router from "./routes/routes";
+import { Toast } from "./components/feedback";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toast />
       <RouterProvider router={router} />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
     </QueryClientProvider>
   </StrictMode>
 );
