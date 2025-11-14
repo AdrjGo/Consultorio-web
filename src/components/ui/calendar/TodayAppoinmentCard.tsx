70ed709ffc0f6fa71a6803ba925f5727c078ee47
AdrjGo <gcadrianj@gmail.com>
2025-11-13 21:45:24 -0400

    feat: update TodayAppoinmentCard to include date filtering and improve layout for appointments

--- src/components/ui/calendar/TodayAppoinmentCard.tsx
+++ src/components/ui/calendar/TodayAppoinmentCard.tsx
@@ -21,9 +21,12 @@ function TodayAppoinmentCard({ onClick }: Props) {
   const [openModal, setOpenModal] = useState(false);
   const [appointmentId, setAppointmentId] = useState<string | null>(null);
 
+  const startDate = dayjs().format("YYYY-MM-DD");
+  const endDate = dayjs().format("YYYY-MM-DD");
+
   const { data } = useGet<AppointmentTypes[]>({
     key: "today-appointments",
-    urlEndpoint: `Appointment/date`,
+    urlEndpoint: `Appointment/date?initialDate=${startDate} 00:00:01&finalDate=${endDate} 23:59:59`,
     message: "Error al obtener las citas de hoy",
   });
 
@@ -64,7 +67,7 @@ function TodayAppoinmentCard({ onClick }: Props) {
   ];
 
   return (
-    <div className="bg-white md:w-1/4 border border-gray-200 rounded-lg p-5 overflow-hidden">
+    <div className="bg-white md:w-1/4 border border-gray-200 rounded-lg p-5 overflow-hidden flex flex-col max-h-[80vh]">
       <h3 className="text-black font-bold text-subtitle flex items-center gap-2">
         <Clock3 className="text-gray-600" /> Citas de hoy
       </h3>
@@ -72,99 +75,101 @@ function TodayAppoinmentCard({ onClick }: Props) {
         {data?.length} Citas programadas
       </span>
 
-      <div className="grid gap-5 mt-3 min-md-max-h-[68dvh] overflow-y-auto">
-        {data
-          ?.slice()
-          .sort(
-            (a, b) =>
-              new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
-          )
+      <section className="overflow-y-auto h-full">
+        <div className="grid gap-4 mt-3">
+          {data
+            ?.slice()
+            .sort(
+              (a, b) =>
+                new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
+            )
 
-          .map((appointment, index) => {
-            const color = statusColors.find(
-              (s) => s.status.toLowerCase() === appointment.status.toLowerCase()
-            );
+            .map((appointment, index) => {
+              const color = statusColors.find(
+                (s) => s.status.toLowerCase() === appointment.status.toLowerCase()
+              );
 
-            return (
-              <section
-                className="border border-gray-200 rounded-md p-3 grid"
-                key={index}
-              >
-                <div className="flex justify-between items-center mb-3">
-                  <span className="font-bold text-normal">
-                    {dayjs(appointment.startDate).format("HH:mm")}
-                  </span>
-                  <span
-                    className={`text-small font-semibold px-2 py-1 rounded-md ${color?.color} ${color?.textColor}`}
-                  >
-                    {appointment.status}
-                  </span>
-                </div>
-
-                <ol className="[&>li]:flex [&>li]:gap-2 items-center text-small grid gap-2">
-                  <li className="font-semibold">
-                    <User strokeWidth={1.25} className="text-gray-600 size-5" />
-                    {appointment.patient.patientPerson.name}
-                  </li>
-                  <li>
-                    <FileText
-                      strokeWidth={1.25}
-                      className="text-gray-600 size-5"
-                    />
-                    {appointment.type.replaceAll("_", " ")}
-                  </li>
-                  <li>
-                    <Notebook
-                      strokeWidth={1.25}
-                      className="text-gray-600 size-5"
-                    />
-                    {appointment.reason}
-                  </li>
-                </ol>
-
-                <div className="flex justify-between gap-3 [&>button]:bg-white [&>button]:text-black [&>button]:border-gray-200 [&>button]:rounded-md [&>button]:p-2 [&>button]:text-small [&>button]:font-semibold [&>button]:hover:bg-gray-200 [&>button]:border [&>button]:focus:bg-gray-100 mt-4">
-                  <Button onClick={() => onClick(appointment.id)}>
-                    <PencilLine className="size-3 mr-2" />
-                    Editar
-                  </Button>
-                  <Button
-                    className="text-white! bg-red-500!"
-                    onClick={() => setOpenModal(true)}
-                  >
-                    <Trash className="size-3 mr-2" />
-                    Eliminar
-                  </Button>
-                </div>
-
-                <Modal
-                  title="Eliminar cita"
-                  desc="Está seguro que desea eliminar esta cita?"
-                  openModal={openModal}
-                  setOpenModal={setOpenModal}
+              return (
+                <section
+                  className="border border-gray-200 rounded-md p-3 grid"
+                  key={index}
                 >
-                  <div className="flex justify-between gap-3 [&>button]:bg-white [&>button]:text-black [&>button]:border-gray-200 [&>button]:rounded-md [&>button]:p-2 [&>button]:text-small [&>button]:font-semibold [&>button]:hover:bg-gray-200 [&>button]:border [&>button]:focus:bg-gray-100 mt-4">
-                    <Button
-                      className="text-small bg-white"
-                      onClick={() => setOpenModal(false)}
+                  <div className="flex justify-between items-center mb-3">
+                    <span className="font-bold text-normal">
+                      {dayjs(appointment.startDate).format("HH:mm")}
+                    </span>
+                    <span
+                      className={`text-small font-semibold px-2 py-1 rounded-md ${color?.color} ${color?.textColor}`}
                     >
-                      Salir
+                      {appointment.status}
+                    </span>
+                  </div>
+
+                  <ol className="[&>li]:flex [&>li]:gap-2 items-center text-small grid gap-2">
+                    <li className="font-semibold">
+                      <User strokeWidth={1.25} className="text-gray-600 size-5" />
+                      {appointment.patient.patientPerson.name}
+                    </li>
+                    <li>
+                      <FileText
+                        strokeWidth={1.25}
+                        className="text-gray-600 size-5"
+                      />
+                      {appointment.type.replaceAll("_", " ")}
+                    </li>
+                    <li>
+                      <Notebook
+                        strokeWidth={1.25}
+                        className="text-gray-600 size-5"
+                      />
+                      {appointment.reason}
+                    </li>
+                  </ol>
+
+                  <div className="flex justify-between gap-3 [&>button]:bg-white [&>button]:text-black [&>button]:border-gray-200 [&>button]:rounded-md [&>button]:p-2 [&>button]:text-small [&>button]:font-semibold [&>button]:hover:bg-gray-200 [&>button]:border [&>button]:focus:bg-gray-100 mt-4">
+                    <Button onClick={() => onClick(appointment.id)}>
+                      <PencilLine className="size-3 mr-2" />
+                      Editar
                     </Button>
                     <Button
                       className="text-white! bg-red-500!"
-                      onClick={() => onClickHandler(appointment.id)}
+                      onClick={() => setOpenModal(true)}
                     >
-                      Confirmar
+                      <Trash className="size-3 mr-2" />
+                      Eliminar
                     </Button>
                   </div>
-                </Modal>
-              </section>
-            );
-          }) ?? (
-          <div className="text-center text-gray-400 font-semibold">
-            No hay citas para hoy
-          </div>
-        )}
-      </div>
+
+                  <Modal
+                    title="Eliminar cita"
+                    desc="Está seguro que desea eliminar esta cita?"
+                    openModal={openModal}
+                    setOpenModal={setOpenModal}
+                  >
+                    <div className="flex justify-between gap-3 [&>button]:bg-white [&>button]:text-black [&>button]:border-gray-200 [&>button]:rounded-md [&>button]:p-2 [&>button]:text-small [&>button]:font-semibold [&>button]:hover:bg-gray-200 [&>button]:border [&>button]:focus:bg-gray-100 mt-4">
+                      <Button
+                        className="text-small bg-white"
+                        onClick={() => setOpenModal(false)}
+                      >
+                        Salir
+                      </Button>
+                      <Button
+                        className="text-white! bg-red-500!"
+                        onClick={() => onClickHandler(appointment.id)}
+                      >
+                        Confirmar
+                      </Button>
+                    </div>
+                  </Modal>
+                </section>
+              );
+            }) ?? (
+              <div className="text-center text-gray-400 font-semibold">
+                No hay citas para hoy
+              </div>
+            )}
+        </div>
+      </section>
     </div>
   );
 }
