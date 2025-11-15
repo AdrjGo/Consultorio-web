1cbccf8cd148dd93b19d6b1ba7ca54f0e3c97869
AdrjGo <gcadrianj@gmail.com>
2025-11-14 22:38:41 -0400

    feat: add useModal to hooks export for improved modal management

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,5 +3,6 @@ import useGet from "./useGet";
 import usePost from "./usePost";
 import useUpdate from "./useUpdate";
 import useDelete from "./useDelete";
+import useModal from "./useModal";
 
-export { useGetUser, useGet, usePost, useUpdate, useDelete };
+export { useGetUser, useGet, usePost, useUpdate, useDelete, useModal };
