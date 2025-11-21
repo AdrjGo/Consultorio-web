b019995ab7dfbf9709e44d045686c3f57cbe4e11
AdrjGo <gcadrianj@gmail.com>
2025-11-20 20:26:52 -0400

    feat: extra compnent props

--- src/components/layout/SectionLayout.tsx
+++ src/components/layout/SectionLayout.tsx
@@ -1,20 +1,28 @@
-
 type Props = {
-    children: React.ReactNode;
-    title: string;
-    description: string;
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  extraComponent?: React.ReactNode;
 };
 
-function SectionLayout({ children, title, description }: Props) {
-    return (
-        <section className="bg-white border border-gray-300 rounded-md p-5 grid gap-4">
-            <div>
-                <h2 className="text-subtitle font-bold">{title}</h2>
-                <p className="text-small text-gray-500">{description}</p>
-            </div>
-            {children}
-        </section>
-    );
+function SectionLayout({
+  children,
+  title,
+  description,
+  extraComponent,
+}: Props) {
+  return (
+    <section className="bg-white border border-gray-300 rounded-md p-5 grid gap-4">
+      <div className="flex justify-between items-center">
+        <div>
+          <h2 className="text-subtitle font-bold">{title}</h2>
+          <p className="text-small text-gray-500">{description}</p>
+        </div>
+        <div>{extraComponent}</div>
+      </div>
+      {children}
+    </section>
+  );
 }
 
-export default SectionLayout
+export default SectionLayout;
